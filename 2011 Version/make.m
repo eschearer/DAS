9dadfb201e2d58dfdf7406ba7b842761cee32b99
jschultz299 <jschultz299@gmail.com>
2020-07-23 11:13:28 -0400

    Update make.m
    Added schearer lab computer to make.m

--- 2011 Version/make.m
+++ 2011 Version/make.m
@@ -10,8 +10,9 @@ function make
         !"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.25.28610\bin\Hostx64\x64\nmake" /nologo
     elseif strcmp(computer, 'FH104K-X2G3')  % Eric's computer
         !"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.26.28801\bin\Hostx64\x64\nmake" /nologo
-    elseif strcmp(computer, '')  % Eric's computer
-        
+    elseif strcmp(computer, 'FHB28-Z640')  % FHB28 Lab computer
+        !"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64\nmake" /nologo
+    elseif strcmp(computer, '')  % Your computer  
     else
        error('In make.m, please add MAKE setup for your computer: %s', computer); 
     end
